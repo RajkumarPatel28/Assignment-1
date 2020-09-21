5cd6b9d17d2eaa139cc784d22de2e9823663e148
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-09-21 15:52:18 -0400

    This is Second Commit

--- app/src/main/java/com/example/rajkumarpatel_ceng319lab1_ex1/FragmentUp.java
+++ app/src/main/java/com/example/rajkumarpatel_ceng319lab1_ex1/FragmentUp.java
@@ -2,47 +2,47 @@ package com.example.rajkumarpatel_ceng319lab1_ex1;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class FragmentUp extends Fragment {
 
-    ListView listview;
+import androidx.fragment.app.ListFragment;
+
+public class FragmentUp extends ListFragment {
 
     String[] activities = new String[]{"AIActivity", "VRActivity"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_up, container, false);
-        listview = (ListView) view.findViewById(R.id.ListView1);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, activities);
-        listview.setAdapter(adapter);
-
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                if (position == 0) {
-                    Intent intent = new Intent(view.getContext(), AIActivity.class);
-                    startActivityForResult(intent, 0);
-                }
-
-                if (position == 1) {
-                    Intent intent = new Intent(view.getContext(), VRActivity.class);
-                    startActivityForResult(intent, 1);
-                }
-
-            }
-        });
+        setListAdapter(adapter);
         return view;
     }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+
+        if (position == 0) {
+            Intent intent = new Intent(v.getContext(), AIActivity.class);
+            startActivity(intent);
+        }
+
+        else {
+            Intent intent = new Intent(v.getContext(), VRActivity.class);
+            startActivity(intent);
+        }
+    }
 }
+
+
+
+
+
+
+
